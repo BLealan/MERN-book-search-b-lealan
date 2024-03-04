073806d248cd073061a21c32c404e4cdcbe3301a
BLealan <143086275+BLealan@users.noreply.github.com>
2024-03-04 17:35:10 +0000

    added schema folder plus content for index.js and typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const typeDefs = `
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        // saveBook [Accepts a book author's array, description, title, bookId, image, and link as parameters]: User
+        removeBook(bookID:ID!): User
+    }
+
+    type User {
+        _id:ID
+        username: String
+        // bookCount
+        savedBooks([Books])
+    }
+    
+    type Book {
+        // bookId book's id value returned from Google's Book API
+        authors([String])
+        description(String)
+        title(String!)
+        image:
+        link(String): 
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+    
+    
+`;
+
+module.exports = typeDefs;
