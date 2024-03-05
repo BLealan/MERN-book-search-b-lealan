a7a9e694c55e988b543aff8acb95bf6bf74e3091
BLealan <143086275+BLealan@users.noreply.github.com>
2024-03-05 14:48:58 +0000

    add mutations.js content

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -1,9 +1,55 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_ME = gql`
-    query me{
-        me {
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password){
             _id
+            username
+        }
+    }
+`;
+
+export const ADD_PROFILE = gql`
+    mutation addProfile($username: String!, $email: String!, $password:String!) {
+        addProfile(username: $username, email: $email, password: $password) {
+            _id
+            username
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookId: ID!) {
+        saveBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
         }
     }
 `;
