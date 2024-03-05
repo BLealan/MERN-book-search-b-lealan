e722d4965a8260548d173aa4625e368799d42e68
BLealan <143086275+BLealan@users.noreply.github.com>
2024-03-05 16:51:10 +0000

    fix to mutations and queries

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -3,8 +3,11 @@ import { gql } from '@apollo/client';
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password){
-            _id
-            username
+            token
+            user {
+                _id
+                username
+            }
         }
     }
 `;
@@ -12,8 +15,11 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
     mutation addProfile($username: String!, $email: String!, $password:String!) {
         addProfile(username: $username, email: $email, password: $password) {
-            _id
-            username
+            token
+            user {
+                _id
+                username
+            }
         }
     }
 `;
