0838a1d10dee2b8b8311d06b3efe78aba851aae8
BLealan <143086275+BLealan@users.noreply.github.com>
2024-03-07 18:02:57 +0000

    address error for heroku

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { authMiddleware } = require('./utils/auth');
 const path = require('path');
-const { typeDefs, resolvers } = require('./Schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const auth = require('./utils/auth');
 
